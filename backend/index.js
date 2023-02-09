0434294f77681a10599506e4df9dc4ad1be1de4e
DiegopVeras <diegopveras02@gmail.com>
2023-02-08 22:34:00 -0300

    feat(form): add db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,34 @@
 const url = "http://localhost:3000/pessoas";
 
+const nodemailer = require('nodemailer');
+
+function sendEmail(destinatario) {
+
+  const transporter = nodemailer.createTransport({
+    host: "diegopveras02@gmail.com",
+    port: 25,
+    secure: false, 
+    auth: {
+      user: "diegopveras02@gmail.com",
+      pass: "Breakfast02@"
+    },
+    tls: { rejectUnauthorized: false }
+  });
+
+  const mailOptions = {
+    from: 'diegopveras02@gmail.com',
+    to: destinatario,
+    subject: 'E-mail enviado usando Node!',
+    text: 'Bem fácil, não? ;)'
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email enviado: ' + info.response);
+    }
+  });
+}
 
 function enviar() {
   // Pegar valores dos campos do form;
@@ -12,15 +41,18 @@ function enviar() {
 
   // Config da requisição;
   request.open("POST", url, true)
-  request.setRequestHeader("Content-Type",  "application/json");
-  
+  request.setRequestHeader("Content-Type", "application/json");
+
   // Montando o objeto pessoa
   const pessoa = {
     nome: nomeValue,
     email: emailValue,
     mensagem: mensagemValue,
   };
-  
-  // Fazendo a requisição;
+
+  // Fazendo a requisição para salvar os dados;
   request.send(JSON.stringify(pessoa))
+
+  //  Fazer uma req para a twilio
+  sendEmail(pessoa.email);
 }
